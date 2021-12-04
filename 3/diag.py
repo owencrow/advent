888fc8294e78564bba58b991877e23ce1bb68665
Owen Crow <owen.crow@gmail.com>
2021-12-03 21:44:49 -0600

    works!

--- 3/diag.py
+++ 3/diag.py
@@ -3,13 +3,22 @@
 with open('input.txt') as f:
     lines = f.read()
 lines = lines.splitlines()
-g = [0] * len(lines[0])
-e = [0] * len(lines[0])
+digits = len(lines[0])
+g = [0] * digits
 count = 0
 
 for l in lines:
     count += 1
-    for i in range(len(l)):
-        print(l,i,l[i])
+    for i in range(digits):
+        #print(l,i,l[i])
         g[i] += int(l[i])
 
+gamma = 0
+epsilon = 0
+for i in range(digits):
+    if g[digits - 1 - i] * 2 > count:
+        gamma += 2**i
+    else:
+        epsilon += 2**i
+
+print(gamma * epsilon, count, g)
